7a7c90c9f6e97a5f7e6005d90a3e46b82dc72fc0
André Luiz <andre.doctors@gmail.com>
2022-04-09 20:32:55 -0300

    Ajuste de pastas e execução do exercicio 2

---
+++ js/exercicio_2_js_externo_coletandados/script.js
@@ -0,0 +1,11 @@
+let nomePessoaVelha = prompt("Digite o nome da pessoa mais velha");
+
+let agePessoaVelha = prompt("Digite a idade da pessoa mais velha");
+
+let nomePessoaNova = prompt("Digite o nome da pessoa mais nova");
+
+let agePessoaNova = prompt("Digite a idade da pessoa mais nova");
+
+let diferenca = agePessoaVelha - agePessoaNova;
+
+alert(`Nome do mais Velho: ${nomePessoaVelha}\nIdade pessoa mais velha: ${agePessoaVelha}\nNome pessoa mais noova: ${nomePessoaNova}\nIdade da pessoa mais nova: ${agePessoaNova}\nDiferença de idade entre elas é: ${diferenca}`)
